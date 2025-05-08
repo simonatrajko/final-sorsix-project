74d88dcb05a77a8fcd143e15403f7441e29bd832
simonatrajko <simona.trajko@students.finki.ukim.mk>
2025-05-08 21:31:00 +0200

    Images rendering and UserDetails end-point and also Search end-point for provider

--- build/resources/main/db/migration/V9__insert_additional_service_categories.sql
+++
@@ -1,8 +0,0 @@
--- V9__insert_additional_service_categories.sql
-
-INSERT INTO service_category (name) VALUES
-                                            ( 'Tutoring'),
-                                            ( 'Hairdressing'),
-                                            ( 'IT Support'),
-                                            ('Carpentry'),
-                                            ( 'Moving Services');

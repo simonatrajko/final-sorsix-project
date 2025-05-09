eec5dee11e0b3c67aa022856f00fff3bcf01c0b8
Kostadin-Pejoski <kostadin.pejoski1@gmail.com>
2025-05-09 11:56:31 +0200

    provider can now delete his services

--- frontend/ServiceConnect/src/app/services/auth.interceptor.ts
+++ frontend/ServiceConnect/src/app/services/auth.interceptor.ts
@@ -5,11 +5,8 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    console.log("intercept method called")
-    
     const accessToken = localStorage.getItem('accessToken');
     if (accessToken) {
-      console.log(accessToken)
       req = req.clone({
         setHeaders: { Authorization: `Bearer ${accessToken}` }
       });
